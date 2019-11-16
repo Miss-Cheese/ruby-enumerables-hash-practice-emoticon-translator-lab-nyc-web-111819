271e171c5c0135f80941dc9316305a683bb84ce1
Natalie Galligan <nat.galligan@gmail.com>
2019-11-16 17:05:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,8 +26,11 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   lib = load_library(file_path)
 
-  lib[:get_emoticon][emoticon]
-
+  if lib[:get_emoticon][emoticon]
+    lib[:get_emoticon][emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
 
 end
 
